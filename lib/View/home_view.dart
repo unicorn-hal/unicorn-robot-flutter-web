9babbab3ad6ce1bab35d6f5f86f1a29cac4eaf22
ryo hasegawa <minecraftnolisio@gmail.com>
2024-12-01 17:10:12 +0900

    add: 各View 判断用Text 追記

--- lib/View/home_view.dart
+++ lib/View/home_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Placeholder(
+      child: Text('THIS IS HOME VIEW'),
+    );
   }
 }
