4932aef1cfd7df3b6879a53e67301a5c026e3325
ryo hasegawa <minecraftnolisio@gmail.com>
2024-12-03 03:18:48 +0900

    add: home login 初期View 追加

--- lib/View/home_view.dart
+++ lib/View/home_view.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:unicorn_robot_flutter_web/Route/router.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text('THIS IS HOME VIEW'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Home View',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                const LoginRoute().go(context);
+              },
+              child: const Text('GO TO Login'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
