0975f09a4eddc72ea4a772688c28b5402c6bac20
ryo hasegawa <minecraftnolisio@gmail.com>
2024-12-05 20:29:46 +0900

    fix authService 使う様に修正

--- lib/Controller/home_controller.dart
+++ lib/Controller/home_controller.dart
@@ -2,9 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:unicorn_robot_flutter_web/Controller/Core/controller_core.dart';
 import 'package:unicorn_robot_flutter_web/Route/router.dart';
+import 'package:unicorn_robot_flutter_web/Service/Firebase/Authentication/authentication_service.dart';
 import 'package:unicorn_robot_flutter_web/Service/Log/log_service.dart';
 
 class HomeController extends ControllerCore {
+  final FirebaseAuthenticationService _firebaseAuthenticationService =
+      FirebaseAuthenticationService();
   BuildContext context;
   HomeController(
     this.context,
@@ -21,14 +24,14 @@ class HomeController extends ControllerCore {
   }
 
   void checkAuthState() {
-    final User? currentRobot = FirebaseAuth.instance.currentUser;
+    final User? currentRobot = _firebaseAuthenticationService.getRobot();
     if (currentRobot == null) {
       const LoginRoute().go(context);
     }
   }
 
   void logout() {
-    FirebaseAuth.instance.signOut();
+    _firebaseAuthenticationService.signOut();
     const LoginRoute().go(context);
   }
 }
