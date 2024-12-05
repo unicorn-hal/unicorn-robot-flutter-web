244fb852d11d340e36a90af07509ed7778c9b1c5
ryo hasegawa <minecraftnolisio@gmail.com>
2024-12-05 10:04:00 +0900

    add: home view controller 追加

--- lib/Controller/home_controller.dart
+++ lib/Controller/home_controller.dart
@@ -1,1 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:unicorn_robot_flutter_web/Route/router.dart';
+import 'package:unicorn_robot_flutter_web/Service/Log/log_service.dart';
 
+class HomeController {
+  BuildContext context;
+  HomeController(
+    this.context,
+  ) {
+    Log.echo('HomeController');
+    initialize();
+  }
+
+  initialize() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      checkAuthState();
+    });
+    Log.echo('initialize');
+  }
+
+  void checkAuthState() {
+    final User? currentRobot = FirebaseAuth.instance.currentUser;
+    if (currentRobot == null) {
+      const LoginRoute().go(context);
+    }
+  }
+
+  void logout() {
+    FirebaseAuth.instance.signOut();
+    const LoginRoute().go(context);
+  }
+}
