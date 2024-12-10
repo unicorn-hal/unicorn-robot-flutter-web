8ddc9660ca099e7ce5ebef24959c2d242f463b00
ryo hasegawa <minecraftnolisio@gmail.com>
2024-12-10 16:26:45 +0900

    add: unicron 各機能仮実装

--- lib/View/home_view.dart
+++ lib/View/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:unicorn_robot_flutter_web/Controller/home_controller.dart';
 import 'package:unicorn_robot_flutter_web/View/Component/CustomWidget/custom_button.dart';
+import 'package:unicorn_robot_flutter_web/View/Component/CustomWidget/custom_text.dart';
+import 'package:unicorn_robot_flutter_web/View/Component/CustomWidget/custom_textfield.dart';
 import 'package:unicorn_robot_flutter_web/gen/assets.gen.dart';
 import 'package:video_player/video_player.dart';
 
@@ -61,6 +63,92 @@ class _HomeViewState extends State<HomeView> {
                 onTap: () {
                   controller.logout();
                 },
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomText(text: '移動通知'),
+                      CustomTextfield(
+                          hintText: 'userID',
+                          controller: controller.movingUserIdController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomTextfield(
+                          hintText: 'latitude',
+                          controller: controller.movingRobotLatitudeController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomTextfield(
+                          hintText: 'longitude',
+                          controller: controller.movingRobotLongitudeController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomButton(
+                          text: 'PUSH',
+                          onTap: () async {
+                            await controller.postMovingUnicorn();
+                          }),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomText(text: '到着通知'),
+                      CustomTextfield(
+                          hintText: 'userID',
+                          controller: controller.arrivalUserIdController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomTextfield(
+                          hintText: 'latitude',
+                          controller: controller.arrivalRobotLatitudeController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomTextfield(
+                          hintText: 'longitude',
+                          controller:
+                              controller.arrivalRobotLongitudeController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomButton(
+                          text: 'PUSH',
+                          onTap: () async {
+                            await controller.postArrivalUnicorn();
+                          }),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomText(text: '完了通知'),
+                      CustomTextfield(
+                          hintText: 'userID',
+                          controller: controller.completeUserIdController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomTextfield(
+                          hintText: 'robotSupportID',
+                          controller:
+                              controller.completeRobotSupportIdController,
+                          width: 150),
+                      const SizedBox(height: 30),
+                      CustomButton(
+                          text: 'PUSH',
+                          onTap: () async {
+                            await controller.postCompleteUnicorn();
+                          }),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 70,
               ),
             ],
           ),
