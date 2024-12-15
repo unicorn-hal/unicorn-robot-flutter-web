a8f866e4ba840a71518bf71c495144960b13aa5b
kakeru-ikeda <kakerumail1451@gmail.com>
2024-12-16 00:18:36 +0900

    wip: 要請ユーザー情報の仮表示

--- lib/View/home_view.dart
+++ lib/View/home_view.dart
@@ -8,8 +8,10 @@ import 'package:unicorn_robot_flutter_web/Model/Data/clock_data.dart';
 import 'package:unicorn_robot_flutter_web/View/Component/CustomWidget/custom_button.dart';
 import 'package:unicorn_robot_flutter_web/View/Component/CustomWidget/custom_progress_indicator.dart';
 import 'package:unicorn_robot_flutter_web/View/Component/CustomWidget/custom_text.dart';
+import 'package:unicorn_robot_flutter_web/View/Component/Parts/user_image_circle.dart';
 import 'package:unicorn_robot_flutter_web/gen/assets.gen.dart';
 import 'package:unicorn_robot_flutter_web/View/Component/Parts/google_map_viewer.dart';
+import 'package:unicorn_robot_flutter_web/gen/colors.gen.dart';
 import 'package:video_player/video_player.dart';
 
 class HomeView extends StatefulWidget {
@@ -38,6 +40,108 @@ class _HomeViewState extends State<HomeView> {
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  Widget _buildUserInfo() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: ColorName.mainColor, width: 2),
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CustomText(
+            text: '要請ユーザー情報',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          const SizedBox(height: 10),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              UserImageCircle(
+                imageSize: 100,
+                imageUrl: _controller.user!.iconImageUrl ?? '',
+              ),
+              const SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildUserInfoRow(
+                    icon: Icons.person,
+                    label:
+                        '名前：${_controller.user!.lastName} ${_controller.user!.firstName}',
+                  ),
+                  _buildUserInfoRow(
+                    icon: Icons.wc,
+                    label: '性別：${_controller.user!.gender.displayName}',
+                  ),
+                  _buildUserInfoRow(
+                    icon: Icons.cake,
+                    label:
+                        '生年月日：${DateFormat('yyyy/MM/dd').format(_controller.user!.birthDate)}',
+                  ),
+                  _buildUserInfoRow(
+                    icon: Icons.height,
+                    label: '身長：${_controller.user!.bodyHeight}cm',
+                  ),
+                  _buildUserInfoRow(
+                    icon: Icons.monitor_weight,
+                    label: '体重：${_controller.user!.bodyWeight}kg',
+                  ),
+                  _buildUserInfoRow(
+                    icon: Icons.work,
+                    label: '職業：${_controller.user!.occupation}',
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          const CustomText(
+            text: '最新の検診結果',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          const SizedBox(height: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildUserInfoRow(
+                icon: Icons.event,
+                label:
+                    '日付：${DateFormat('yyyy/MM/dd').format(_controller.healthCheckUpList!.last.date)}',
+              ),
+              _buildUserInfoRow(
+                icon: Icons.thermostat,
+                label:
+                    '体温：${_controller.healthCheckUpList!.last.bodyTemperature}℃',
+              ),
+              _buildUserInfoRow(
+                icon: Icons.favorite,
+                label:
+                    '血圧：${_controller.healthCheckUpList!.last.bloodPressure}',
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUserInfoRow({required IconData icon, required String label}) {
+    return Row(
+      children: [
+        Icon(icon),
+        SizedBox(width: 8),
+        CustomText(
+          text: label,
+        ),
+      ],
+    );
   }
 
   @override
@@ -126,25 +230,7 @@ class _HomeViewState extends State<HomeView> {
                             alignment: Alignment.topLeft,
                             child: emergencyQueue == null
                                 ? const SizedBox()
-                                : Column(
-                                    children: [
-                                      const CustomText(
-                                        text: '要請ユーザー情報',
-                                      ),
-                                      CustomText(
-                                        text:
-                                            '名前：${_controller.user!.lastName} ${_controller.user!.firstName}',
-                                      ),
-                                      CustomText(
-                                        text:
-                                            '性別：${_controller.user!.gender.displayName}',
-                                      ),
-                                      CustomText(
-                                        text:
-                                            '生年月日：${DateFormat('yyyy/MM/dd').format(_controller.user!.birthDate)}',
-                                      ),
-                                    ],
-                                  ),
+                                : _buildUserInfo(),
                           ),
                         ),
 
@@ -166,7 +252,8 @@ class _HomeViewState extends State<HomeView> {
                         // ),
                       ],
                     );
-                  }),
+                  },
+                ),
         ),
       ),
     );
